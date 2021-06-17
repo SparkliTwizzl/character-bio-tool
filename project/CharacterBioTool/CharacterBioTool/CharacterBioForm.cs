4a863cbf7de7000134c7fa80996141ea3eee3ca1
SparkliTwizzl <sparklitwizzl@gmail.com>
2021-06-16 20:14:55 -0600

    Added comments, text box font, new fields, fixed CapCaseToFirstCaps bug

--- project/CharacterBioTool/CharacterBioTool/CharacterBioForm.cs
+++ project/CharacterBioTool/CharacterBioTool/CharacterBioForm.cs
@@ -15,14 +15,14 @@ namespace CharacterBioTool
 
 	public partial class CharacterBioForm : Form
 	{
-		// field class wrappers for winform controls to bundle labels and other functionality
-
 		public struct Dimensions
 		{
 			public int width;
 			public int height;
 		}
 
+		// field class wrappers for winform controls to bundle labels and other functionality
+		// fields are limited to one functional control each
 		public enum FIELD_TYPE
 		{
 			BASIC,
@@ -105,20 +105,14 @@ namespace CharacterBioTool
 				: base(_form, _labelText)
 			{
 				// create button control
-				//button = new Button();
-				//button.Location = new Point(origin.X, label.Bottom + form.fieldControlSpacingY);
-				//button.Text = _buttonText;
 				control = new Button();
 				control.Location = new Point(origin.X, label.Bottom + form.fieldControlSpacingY);
 				control.Text = _buttonText;
 
 				// add button to form controls
-				//form.Controls.Add(button);
 				form.Controls.Add(control);
 
 				// store extents
-				//right = Math.Max(right, button.Right);
-				//bottom = button.Bottom;
 				right = Math.Max(right, control.Right);
 				bottom = control.Bottom;
 				CalculateSize();
@@ -126,20 +120,17 @@ namespace CharacterBioTool
 
 			~ButtonField()
 			{
-				//form.Controls.Remove(button);
 				form.Controls.Remove(control);
 			}
 
 			public override void SetForeColor(Color _color)
 			{
 				base.SetForeColor(_color);
-				//button.ForeColor = _color;
 				control.ForeColor = _color;
 			}
 			public override void SetMidColor(Color _color)
 			{
 				base.SetMidColor(_color);
-				//button.BackColor = _color;
 				control.BackColor = _color;
 			}
 			public override void SetBackColor(Color _color)
@@ -156,19 +147,14 @@ namespace CharacterBioTool
 				: base(_form, _labelText)
 			{
 				// create text box control
-				//textBox = new TextBox();
-				//textBox.Location = new Point(origin.X, label.Bottom + form.fieldControlSpacingY);
 				control = new TextBox();
-				control.Font = CharacterBioForm.labelFont;
+				control.Font = CharacterBioForm.textBoxFont;
 				control.Location = new Point(origin.X, label.Bottom + form.fieldControlSpacingY);
 
 				// add text box to form controls
-				//form.Controls.Add(textBox);
 				form.Controls.Add(control);
 
 				// store extents
-				//right = Math.Max(right, textBox.Right);
-				//bottom = textBox.Bottom;
 				right = Math.Max(right, control.Right);
 				bottom = control.Bottom;
 				CalculateSize();
@@ -176,20 +162,17 @@ namespace CharacterBioTool
 
 			~TextBoxField()
 			{
-				//form.Controls.Remove(textBox);
 				form.Controls.Remove(control);
 			}
 
 			public override void SetForeColor(Color _color)
 			{
 				base.SetForeColor(_color);
-				//textBox.ForeColor = _color;
 				control.ForeColor = _color;
 			}
 			public override void SetMidColor(Color _color)
 			{
 				base.SetMidColor(_color);
-				//textBox.BackColor = _color;
 				control.BackColor = _color;
 			}
 			public override void SetBackColor(Color _color)
@@ -201,29 +184,39 @@ namespace CharacterBioTool
 
 
 		// helper functions
+
+		// generates a Color with the specified grey tone
 		public static Color GreyTone(int _tone) { return Color.FromArgb(_tone, _tone, _tone); }
-		public static String CapCaseToTitleCase(String _str)
+
+		// converts a CAP_CASE string to First Capitals Case
+		public static String CapCaseToFirstCaps(String _str)
 		{
-			_str = _str.ToLower();
 			StringBuilder str = new StringBuilder();
 
-			if (_str[0] >= 'a' && _str[0] <= 'z')
-				str.Append(_str[0].ToString().ToUpper());
-			else if (_str[0] == '_')
+			// convert string to lowercase first so each letter doesn't have to be converted separately
+			_str = _str.ToLower();
+			// handle first letter separately, since there is no previous letter to compare it to
+			// replace underscore with space
+			if (_str[0] == '_')
 				str.Append(' ');
+			// capitalize char
 			else
-				str.Append(_str[0]);
+				str.Append(_str[0].ToString().ToUpper());
 
+			// handle rest of string
 			for (int i = 1; i < _str.Length; ++i)
 			{
-				if (_str[i - 1] == '_')
+				// replace underscores with spaces
+				if (_str[i] == '_')
 				{
-					str.Append(_str[i].ToString().ToUpper());
+					str.Append(' ');
 				}
 				else
 				{
-					if (_str[i] == '_')
-						str.Append(' ');
+					// if previous char is underscore, capitalize this char
+					if (_str[i - 1] == '_')
+						str.Append(_str[i].ToString().ToUpper());
+					// otherwise, add char unmodified
 					else
 						str.Append(_str[i]);
 				}
@@ -237,7 +230,8 @@ namespace CharacterBioTool
 		// form variables
 
 		// fonts
-		static Font labelFont = new Font("Roboto", 10);
+		static Font labelFont = new Font("Roboto", 10, FontStyle.Bold);
+		static Font textBoxFont = new Font("Roboto", 10);
 
 		// color modes
 		enum COLOR_MODE
@@ -303,8 +297,19 @@ namespace CharacterBioTool
 			LITERAL_AGE,
 			PHYSICAL_AGE,
 			APPARENT_AGE,
+			HEIGHT,
+			WEIGHT,
+			BUILD,
+			SKIN_COLOR,
+			EYE_COLOR,
+			HAIR_LENGTH,
+			HAIR_STYLE,
+			HAIR_COLOR,
+			FACIAL_HAIR_LENGTH,
+			FACIAL_HAIR_STYLE,
+			FACIAL_HAIR_COLOR,
+			ATTIRE,
 		}
-
 		FIELD_TYPE[] fields = new FIELD_TYPE[]
 		{
 			FIELD_TYPE.BUTTON,
@@ -317,30 +322,20 @@ namespace CharacterBioTool
 			FIELD_TYPE.TEXT_BOX,
 			FIELD_TYPE.TEXT_BOX,
 			FIELD_TYPE.TEXT_BOX,
+			FIELD_TYPE.TEXT_BOX,
+			FIELD_TYPE.TEXT_BOX,
+			FIELD_TYPE.TEXT_BOX,
+			FIELD_TYPE.TEXT_BOX,
+			FIELD_TYPE.TEXT_BOX,
+			FIELD_TYPE.TEXT_BOX,
+			FIELD_TYPE.TEXT_BOX,
+			FIELD_TYPE.TEXT_BOX,
+			FIELD_TYPE.TEXT_BOX,
+			FIELD_TYPE.TEXT_BOX,
+			FIELD_TYPE.TEXT_BOX,
+			FIELD_TYPE.TEXT_BOX,
+			FIELD_TYPE.TEXT_BOX,
 		};
-
-
-		//ButtonField		colorModeButton;
-		//TextBoxField	nameBox;
-		//TextBoxField	nicknameBox;
-		//TextBoxField	raceBox;
-		//TextBoxField	genderBox;
-		//TextBoxField	sexBox;
-		//TextBoxField	birthDateBox;
-		//TextBoxField	deathDateBox;
-		//TextBoxField	literalAgeBox;
-		//TextBoxField	physicalAgeBox;
-		//TextBoxField	apparentAgeBox;
-		/*
-			height
-			weight
-			build
-			skin color
-			eye color
-			hair
-			facial hair
-			attire
-		*/
 
 
 
@@ -349,60 +344,51 @@ namespace CharacterBioTool
 		{
 			InitializeComponent();
 
+			// create lambda to make accessing fields in list easier
+			Func<FIELD_NAME, Field> GetField = _fieldName => fieldList[Convert.ToInt32(_fieldName)];
+
+			// create color mode lambdas
+			Func<COLOR_MODE, ColorPalette> GetPalette = _colorMode => palettes[(int)_colorMode];
+			Action<COLOR_MODE> SetColorMode = _colorMode =>
+			{
+				ColorPalette palette = GetPalette(colorMode);
+				SetFormColors(palette);
+				GetField(FIELD_NAME.COLOR_MODE).control.Text = Convert.ToString(colorMode);
+			};
+
 			// init fields and add them to form
-			//AddField(colorModeButton	= new ButtonField(this, "Color Mode", ""));
-			//AddField(nameBox			= new TextBoxField(this, "Name"));
-			//AddField(nicknameBox		= new TextBoxField(this, "Nickname"));
-			//AddField(raceBox			= new TextBoxField(this, "Race"));
-			//AddField(genderBox			= new TextBoxField(this, "Gender"));
-			//AddField(sexBox				= new TextBoxField(this, "Sex"));
-			//AddField(birthDateBox		= new TextBoxField(this, "Birth Date"));
-			//AddField(deathDateBox		= new TextBoxField(this, "Death Date"));
-			//AddField(literalAgeBox		= new TextBoxField(this, "Literal Age"));
-			//AddField(physicalAgeBox		= new TextBoxField(this, "Physical Age"));
-			//AddField(apparentAgeBox		= new TextBoxField(this, "Apparent Age"));
 			for (int i = 0; i < fields.Length; ++i)
 			{
 				switch (fields[i])
 				{
 					default: break;
 					case FIELD_TYPE.BASIC:
-						AddField(new Field(this, CapCaseToTitleCase(Convert.ToString((FIELD_NAME)i))));
+						AddField(new Field(this, CapCaseToFirstCaps(Convert.ToString((FIELD_NAME)i))));
 						break;
 					case FIELD_TYPE.BUTTON:
-						AddField(new ButtonField(this, CapCaseToTitleCase(Convert.ToString((FIELD_NAME)i)), ""));
+						AddField(new ButtonField(this, CapCaseToFirstCaps(Convert.ToString((FIELD_NAME)i)), ""));
 						break;
 					case FIELD_TYPE.TEXT_BOX:
-						AddField(new TextBoxField(this, CapCaseToTitleCase(Convert.ToString((FIELD_NAME)i))));
+						AddField(new TextBoxField(this, CapCaseToFirstCaps(Convert.ToString((FIELD_NAME)i))));
 						break;
 				}
 			}
 
-
-			// create color mode lambdas
-			Func<COLOR_MODE, ColorPalette> GetPalette = _colorMode => palettes[(int)colorMode];
-			Action<COLOR_MODE> SetColorMode = _colorMode =>
-			{
-				ColorPalette palette = GetPalette(colorMode);
-				SetFormColors(palette);
-				//colorModeButton.button.Text = Convert.ToString(colorMode);
-				fieldList[Convert.ToInt32(FIELD_NAME.COLOR_MODE)].control.Text = Convert.ToString(colorMode);
-			};
-
-			// set initial color mode
-			SetColorMode(colorMode);
-
-
 			// set field custom behaviors
-
 			// set color mode button behavior with event handler lambda
-			//colorModeButton.button.Click += (obj, eventArgs) =>
-			fieldList[Convert.ToInt32(FIELD_NAME.COLOR_MODE)].control.Click += (obj, eventArgs) =>
+			GetField(FIELD_NAME.COLOR_MODE).control.Click += (obj, eventArgs) =>
 			{
 				this.ActiveControl = null; // defocus button after clicking it
 				colorMode = (colorMode == COLOR_MODE.MAX) ? (COLOR_MODE.MIN) : (colorMode + 1);
 				SetColorMode(colorMode);
 			};
+
+
+			// resize window
+			Size = new Size(1920, 1080);
+
+			// set initial color mode
+			SetColorMode(colorMode);
 		}
 
 		// functions to manage fields
